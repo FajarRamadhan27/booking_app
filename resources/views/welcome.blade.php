255add9e14ae5d080d5844724e6e8202cbf50e13
FajarRamadhan27 <fajarramadhan220@gmail.com>
2022-08-27 22:50:36 +0700

    Adding react material ui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,11 @@
         <title>Booking App</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
