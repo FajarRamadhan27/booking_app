e3c87589a3f9933e63f2809609af3126fd873e36
FajarRamadhan27 <fajarramadhan220@gmail.com>
2022-09-03 17:38:53 +0700

    create event facility resources

---
+++ database/migrations/2022_09_03_103140_create_event_schedule_facilities_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventScheduleFacilitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event_schedule_facilities', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('event_schedule_id')->constrained('event_schedules');
+            $table->foreignId('facilities_id')->constrained('facilities');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event_schedule_facilities');
+    }
+}
