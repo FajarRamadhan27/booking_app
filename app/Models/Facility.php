eb1d55d4cd84a2dd7e5e3a67ffb00b4d0d26b186
FajarRamadhan27 <fajarramadhan220@gmail.com>
2022-09-03 17:37:35 +0700

    create facility resources

---
+++ app/Models/Facility.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Facility extends Model
+{
+    use HasFactory;
+
+    public const STUDIO = 'STUDIO';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['id', 'title', 'type'];
+
+    /**
+     * Get event schedule.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function schedules()
+    {
+        return $this->hasOne(EventScheduleFacility::class);
+    }
+}
