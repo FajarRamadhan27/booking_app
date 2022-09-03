b25c0769f4cab3704924395d1aea4347ae126140
FajarRamadhan27 <fajarramadhan220@gmail.com>
2022-09-03 16:23:26 +0700

    change appbar backgrround color

--- resources/js/components/CustomAppBar.js
+++ resources/js/components/CustomAppBar.js
@@ -1,11 +1,10 @@
-import Slide from '@mui/material/Slide';
+import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import { Toolbar, Typography } from '@mui/material';
-import React from 'react';
 
 function CustomAppBar() {
   return (
-    <AppBar sx={{ backgroundColor:'GrayText'}}>
+    <AppBar>
       <Toolbar variant='dense'>
         <Typography variant="body1" component="div">
           Booking App
