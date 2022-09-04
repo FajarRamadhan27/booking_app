c9a2f339a821fc916ba9f9529cadb7027c8413d2
FajarRamadhan27 <fajarramadhan220@gmail.com>
2022-09-04 12:47:49 +0700

    adjust booking completed layout

--- resources/js/components/forms/BookingComplete.js
+++ resources/js/components/forms/BookingComplete.js
@@ -9,27 +9,33 @@ function BookingComplete() {
     const { selectedEventSchedule } = useSelector((state) => state.eventSchedules)
 
     return (
-        <div className='p-5 flex flex-col justify-center items-center h-full'>
+        <div className='p-5 flex flex-col justify-between items-center h-full'>
             <Typography component="div" variant="h6" fontSize={16} align={"center"}>
                 {`${selectedEventSchedule.title}(Fajar Ramadhan : fajarramadhan220@gmail.com)`}
             </Typography>
-            <Grid justifyContent={"center"} container sx={{ mt: 1 }}>
-                {
-                    selectedSeats.map(seat => {
-                        return <EventSeat seat={seat}/>
-                    })
-                }
-            </Grid>
-            <Typography variant='h6'>{'Total Price: $' + totalPurchases }</Typography>
-            <div className="p-3 my-2 flex items-center justify-center">
-                <DoneAllIcon fontSize="large" color="success" sx={{ mr: 2 }}/>
-                <Typography component="div" variant="h6" align={"center"} color={'green'}>
-                    SEAT BOOKED
+
+            <div className='flex flex-col justify-center items-center'>
+                <Grid justifyContent={"center"} container sx={{ mt: 1 }}>
+                    {
+                        selectedSeats.map(seat => {
+                            return <EventSeat seat={seat}/>
+                        })
+                    }
+                </Grid>
+                <Typography variant='h6'>{'Total Price: $' + totalPurchases }</Typography>
+            </div>
+
+            <div>
+                <div className="p-3 my-2 flex items-center justify-center">
+                    <DoneAllIcon fontSize="large" color="success" sx={{ mr: 2 }}/>
+                    <Typography component="div" variant="h6" align={"center"} color={'green'}>
+                        SEAT BOOKED
+                    </Typography>
+                </div>
+                <Typography component="div" variant="body2"align={"center"}>
+                    {`Note: - `}
                 </Typography>
             </div>
-            <Typography component="div" variant="body2"align={"center"}>
-                {`Note: - `}
-            </Typography>
         </div>
     )
 }
